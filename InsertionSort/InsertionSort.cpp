5efc6b7f96dedbb9501ea474fa12fa7977985b61
FikriBaihaqiAdriano <fikribaihaqiadriano@gmail.com>
2023-03-24 16:26:22 +0700

    Membuat tampilan susunan data elemen array

--- InsertionSort/InsertionSort.cpp
+++ InsertionSort/InsertionSort.cpp
@@ -16,3 +16,7 @@ void input() {
 			cout << "\nArray yang anda masukkan maksimal 20 elemen.\n";
 		}
 	}
+	cout << endl;
+	cout << "======================" << endl;
+	cout << "Masukkan Element Array" << endl;
+	cout << "======================" << endl;
